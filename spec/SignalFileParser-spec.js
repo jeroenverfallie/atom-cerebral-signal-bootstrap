89e3efe936473e72162b2e2fda03e51a437782f0
Jeroen Verfallie <verfalliejeroen@gmail.com>
2016-03-14 23:23:42 +0100

    feat(core): Core functionality and first commit

---
+++ spec/SignalFileParser-spec.js
@@ -0,0 +1,257 @@
+'use babel';
+
+import SignalFileParser from '../lib/SignalFileParser.js';
+
+var content = `import doOtherStuff from '../../';
+import random from '../../';
+
+export default [
+    setStuff,
+    doOtherStuff(some, args), {
+        success: [...doMoreStuff, findMore(), {success: [ok, ...test]}],
+        error: [otherThings]
+    },
+    ...andOthers(more, args),
+    somethingElse
+];`;
+
+describe('test', () => {
+
+    it('should fail on wrong files', () => {
+        const content = `
+            export default [];
+        `;
+
+        const result = SignalFileParser.parse(content);
+        
+        expect(result).toEqual(false);
+    });
+
+    it('should parse actions', () => {
+        const content = `
+            export default [
+                actionA,
+                actionB
+            ];
+        `;
+
+        const expected = [{
+            type: 'action',
+            name: 'actionA'
+        }, {
+            type: 'action',
+            name: 'actionB'
+        }];
+
+        const result = SignalFileParser.parse(content);
+
+        expect(result).toEqual(expected);
+    });
+
+    it('should parse factories', () => {
+        const content = `
+            export default [
+                factoryA(param),
+                factoryB('test'),
+                ...factoryC(namespaced.param)
+            ];
+        `;
+
+        const expected = [{
+            type: 'factory',
+            name: 'factoryA'
+        }, {
+            type: 'factory',
+            name: 'factoryB'
+        }, {
+            type: 'factory',
+            name: 'factoryC'
+        }];
+
+        const result = SignalFileParser.parse(content);
+
+        expect(result).toEqual(expected);
+    });
+
+    it('should parse chains', () => {
+        const content = `
+            export default [
+                ...chainA,
+                ...chainB
+            ];
+        `;
+
+        const expected = [{
+            type: 'chain',
+            name: 'chainA'
+        }, {
+            type: 'chain',
+            name: 'chainB'
+        }];
+
+        const result = SignalFileParser.parse(content);
+
+        expect(result).toEqual(expected);
+    });
+
+    it('should parse outputs', () => {
+        const content = `
+            export default [
+                actionA,
+                actionB, {
+                    outputA: [
+                        ...chainA
+                    ],
+                    outputB: []
+                },
+                factoryA()
+            ];
+        `;
+
+        const expected = [{
+            type: 'action',
+            name: 'actionA'
+        }, {
+            type: 'action',
+            name: 'actionB',
+            outputs: ['outputA', 'outputB']
+        }, {
+            type: 'chain',
+            name: 'chainA'
+        }, {
+            type: 'factory',
+            name: 'factoryA'
+        }];
+
+        const result = SignalFileParser.parse(content);
+
+        expect(result).toEqual(expected);
+    });
+
+    it('should parse imports', () => {
+        const content = `
+            import actionB from '../actions/actionA.js';
+            import factoryA from '../factories/factoryA.js';
+
+            export default [
+                actionA,
+                actionB, {
+                    outputA: [
+                        ...chainA
+                    ],
+                    outputB: []
+                },
+                factoryA()
+            ];
+        `;
+
+        const expected = [{
+            type: 'action',
+            name: 'actionA'
+        }, {
+            type: 'chain',
+            name: 'chainA'
+        }, {
+            type: 'import',
+            name: 'actionB',
+            outputs: ['outputA', 'outputB']
+        }, {
+            type: 'import',
+            name: 'factoryA'
+        }];
+
+        const result = SignalFileParser.parse(content);
+
+        expect(result).toEqual(expected);
+    });
+
+
+    it('should parse deep nested things..uh', () => {
+        const content = `
+            import chainB from '../chains/chainB.js';
+
+            export default [
+                [
+                    actionA,
+                    actionB, {
+                        outputA: [
+                            ...chainA
+                        ],
+                        outputB: [
+                            factoryB(), {
+                                outputA: [
+                                    actionA,
+                                    actionC, {
+                                        outputB: [
+                                            factoryC(), ...chainB
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ],
+                factoryA()
+            ];
+        `;
+
+        const expected = [{
+            type: 'action',
+            name: 'actionA'
+        }, {
+            type: 'action',
+            name: 'actionB',
+            outputs: ['outputA', 'outputB']
+        }, {
+            type: 'action',
+            name: 'actionC',
+            outputs: ['outputB']
+        }, {
+            type: 'chain',
+            name: 'chainA'
+        }, {
+            type: 'factory',
+            name: 'factoryA'
+        }, {
+            type: 'factory',
+            name: 'factoryB',
+            outputs: ['outputA']
+        }, {
+            type: 'factory',
+            name: 'factoryC'
+        }, {
+            type: 'import',
+            name: 'chainB'
+        }];
+
+        const result = SignalFileParser.parse(content);
+
+        expect(result).toEqual(expected);
+    });
+
+
+// it('should parse everything', () => {
+//     const content = `
+//         import doOtherStuff from '../../';
+//         import random from '../../';
+//
+//         export default [
+//             setStuff,
+//             doOtherStuff(some, args), {
+//                 success: [...doMoreStuff, findMore(), {success: [ok, ...test]}],
+//                 error: [otherThings]
+//             },
+//             ...andOthers(more, args),
+//             somethingElse
+//         ];
+//     `;
+//
+//     const result = SignalFileParser.parse(content);
+//
+//     console.log(result);
+//
+//
+//     expect('life').toBe('lidfe');
+// });
+
+});
